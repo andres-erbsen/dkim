3466a25c686dede3618b279d4f8d008b8299cd4f
Andres Erbsen <andreser@yahoo-inc.com>
2015-08-17 19:42:46 -0700

    BROKEN multiple signature support

--- dkim.go
+++ dkim.go
@@ -63,70 +63,69 @@ func extractHeaders(headers []string, names []string) []string {
 // ParseAndVerify parses the e-mail, searches for a DKIM signature, verifies
 // the signature, and returns the resulting verified e-mail. Results in an
 // error if there is no valid signature.
-func ParseAndVerify(mail string, mode VerificationMode, dnsClient DNSClient) (*VerifiedEmail, error) {
+func ParseAndVerify(mail string, mode VerificationMode, dnsClient DNSClient) (veml *VerifiedEmail, err error) {
 	email := parseEmail(mail)
 
-	var signatureHeader string
 	for _, header := range email.headers {
 		// we don't support DKIM-Signature headers signing other DKIM-Signature
 		// headers
 		if isSignatureHeader(header) {
-			// if signatureHeader != "" {
-			// 	return nil, errors.New("multiple DKIM headers")
-			// }
-			signatureHeader = header
+			var signature *Signature
+			signature, err = parseSignature(header)
+			if err != nil {
+				continue
+			}
+
+			if mode == Complete {
+				h := signature.algo.hasher()
+				body := signature.canon.body(email.body)
+				h.Write([]byte(body))
+
+				if !bytes.Equal(signature.bodyHash, h.Sum(nil)) {
+					err = errors.New("body hash does not match")
+					continue
+				}
+			}
+
+			var txtRecords []string
+			txtRecords, err = dnsClient.LookupTxt(signature.txtRecordName())
+			if err != nil {
+				continue
+			}
+
+			signedHeaders := extractHeaders(email.headers, signature.headerNames)
+
+			h := signature.algo.hasher()
+			for _, header := range signedHeaders {
+				header = signature.canon.header(header)
+				h.Write([]byte(header))
+			}
+			header := signature.canon.header(signature.trimmedHeader)
+			h.Write([]byte(header))
+
+			headersHash := h.Sum(nil)
+
+			found := false
+			for _, txtRecord := range txtRecords {
+				pubkey := parsePubkey(txtRecord)
+				if err = signature.algo.checkSig(pubkey.key, headersHash, signature.signature); err == nil {
+					found = true
+				}
+			}
+			if !found {
+				err = errors.New("no valid DKIM signature")
+				continue
+			}
+
+			return &VerifiedEmail{
+				email:     email,
+				Signature: signature,
+				Headers:   signedHeaders,
+			}, nil
 		}
 	}
-	if signatureHeader == "" {
-		return nil, errors.New("no DKIM header found")
+	if err == nil {
+		err = errors.New("no DKIM header found")
 	}
-
-	signature, err := parseSignature(signatureHeader)
-	if err != nil {
-		return nil, err
-	}
-
-	if mode == Complete {
-		h := signature.algo.hasher()
-		body := signature.canon.body(email.body)
-		h.Write([]byte(body))
-
-		if !bytes.Equal(signature.bodyHash, h.Sum(nil)) {
-			return nil, errors.New("body hash does not match")
-		}
-	}
-
-	txtRecords, err := dnsClient.LookupTxt(signature.txtRecordName())
-	if err != nil {
-		return nil, err
-	}
-
-	signedHeaders := extractHeaders(email.headers, signature.headerNames)
-
-	h := signature.algo.hasher()
-	for _, header := range signedHeaders {
-		header = signature.canon.header(header)
-		h.Write([]byte(header))
-	}
-	header := signature.canon.header(signature.trimmedHeader)
-	h.Write([]byte(header))
-
-	headersHash := h.Sum(nil)
-
-	found := false
-	for _, txtRecord := range txtRecords {
-		pubkey := parsePubkey(txtRecord)
-		if err := signature.algo.checkSig(pubkey.key, headersHash, signature.signature); err == nil {
-			found = true
-		}
-	}
-	if !found {
-		return nil, errors.New("no valid DKIM signature")
-	}
-
-	return &VerifiedEmail{
-		email:     email,
-		Signature: signature,
-		Headers:   signedHeaders,
-	}, nil
+	return nil, err
 }
--- dkim_test.go
+++ dkim_test.go
@@ -23,6 +23,7 @@ var client = &fakeDnsClient{
 		"google._domainkey.vandenhooff.name.": []string{
 			`v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCl2Qrp5KF1uJnQSO0YuwInVPISQRrUciXtg/5hnQl6ed+UmYvWreLyuiyaiSd9X9Zu+aZQoeKm67HCxSMpC6G2ar0NludsXW69QdfzUpB5I6fzaLW8rl/RyeGkiQ3D66kvadK1wlNfUI7Dt9WtnUs8AFz/15xvODzgTMFJDiAcAwIDAQAB`,
 		},
+		"ginc1024._domainkey.yahoo-inc.com.": []string{"k=rsa; t=y; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC1vQTaStWYO5FZFCi5ar2pczho8AzfbC9zrAWQrWE+bdarcgE/Zf1/e3BMXvb1Vht6W+eC3JghC1vfeH+Rf/qSWYC/GjhZZulGNjy1beyuu7rLtYJpG3V1Cuqj0zkjsYn+KqQ2KnubGu4KBD1PJlBGCnVCRvAll953Ucl7hJpmgQIDAQAB;"},
 	},
 }
 
@@ -158,5 +159,75 @@ func TestHeadersOnly(t *testing.T) {
 
 	if email.CanonHeaders() != headersOnly {
 		t.Errorf("unexpected canonical form")
+	}
+}
+
+var yahooIncDKIMtest = strings.Replace(`X-Apparently-To: andreser@yahoo-inc.com; Mon, 17 Aug 2015 22:49:25 +0000
+Return-Path: <andreser@yahoo-inc.com>
+Received-SPF: pass (domain of yahoo-inc.com designates 216.145.54.109 as permitted sender)
+X-YMailISG: mjZhcf4WLDsyM65yWRfgyfO_lZT.dRW6ZkL0mQ36QKSZ1wt8
+ norPyPfS_RaocAsatZMUc76bWB9uuFubtxIu.6wHOaop_IvkFzIxMpIj0qV.
+ Lrx.L7iOLJ2Y5WVt6viLV7QS58O_2NzGwj3OIQL5EkGvSAZntHzX6fwew2_o
+ mtpmgrO9DKSOmSxs0mI1hgXdqr2U2oqrtF9ibc4Z2cFMaZ4R1JeYcprQW9Xu
+ X0YqkidSky.VEpst35uNTE.OMGZrIFHPzaKfF5GarnIJGSqhk.5NMjq_Bywg
+ 5LYpX9AoXaCFOQd0Tzp4raM0IUmhBRaGPPXUBbzqovVvuLdJ.clh6.kYtv_F
+ 5aNQtHP5cNqhPTooi1c_mZlh6phP12PMUVdx9WdfEmvVaN1Jumay.SzOtTPh
+ 89IA7pgAferCuLh5f_9lEkYLkFomW4SRwexAbpdfwm1R1CYprsZMQ1YhFZI3
+ GinHyEiPUo48hxgTJgWIuv0oiCoDzd8exD5.u0ZW6Ztvy3UVvogbGCJ6KvXy
+ 7CT1iwdHcoCiGcoE9e7zEqZdH7GftkZGobaX83r3bzhhc0GVMmY29fB4BnZj
+ suHtpK.Cx7vY.hJvV_R_.QH5npxcM8ptVFLgkNW6tBzqF9GnbWtr7v2ERGjn
+ hewHjiEQAGbay6c19tw.3s0SEEhb0BdbxeGajeqNJhYLC8j18hRQR67oWyFF
+ LON7S1cfRM2sQKVWW4K0I7KMad7FrxEi6VJdfIVD8gLMW7uhlkowqOE9rhtj
+ 042FEnYc7kcrvL58Bj8v9TY3Z2Nl8HXifr6dGK_Kw9HK79We3O00cdZSWASu
+ R8pA_AB40d80d82.0crHu0oFFX6KFT8xkAipyIvPhK4bZz7r.NnBD1ZKq7ZF
+ TpBmxt0hbxWy_Qkz1M9BrzrGbbeSAFhAyyZqoPYsWy8FN5U3jzU.ZQygaK.E
+ DT18hIHBF2qN4R3JLVxA7zX1OfxL24UlPvuPaAERm9Wq4WRagcK7ysJt7.9b
+ WskH.vySl_.3mtF7yBFXOR_7_aIM54djcILP_MGhqEjJVbPp12KmbQ51cD_o
+ 76mHVraxIkOZV0eVal8V9QwIaAbbb9caFJcySJdUSIVvojxd6fneN83jCsD.
+ Df0Iz4J2pF0BYiVnY4.MIhUSZZtCjxBAK4roNSvdyVDEQdYPiJpQHoBIDCnj
+ mgQCRPWOCRXaaDMnFvBzJJ7_z04R5rB3vFg65xsBN0wyeDX1veLLsMHChAbp
+ 8RPEQFrsqmFFrXXRODtacXX1ZOZV1tTI
+X-Originating-IP: [216.145.54.109]
+Authentication-Results: mta2007.corp.mail.ne1.yahoo.com  from=yahoo-inc.com; domainkeys=neutral (no sig);  from=yahoo-inc.com; dkim=pass (ok)
+Received: from 127.0.0.1  (EHLO mrout4.yahoo.com) (216.145.54.109)
+  by mta2007.corp.mail.ne1.yahoo.com with SMTPS; Mon, 17 Aug 2015 22:49:25 +0000
+Received: from omp1017.mail.ne1.yahoo.com (omp1017.mail.ne1.yahoo.com [98.138.89.161])
+	by mrout4.yahoo.com (8.14.9/8.14.9/y.out) with ESMTP id t7HMn6pT004450
+	(version=TLSv1/SSLv3 cipher=DHE-RSA-CAMELLIA256-SHA bits=256 verify=NO)
+	for <andreser@yahoo-inc.com>; Mon, 17 Aug 2015 15:49:06 -0700 (PDT)
+Received: (qmail 15334 invoked by uid 1000); 17 Aug 2015 22:49:06 -0000
+DKIM-Signature: v=1; a=rsa-sha256; c=relaxed/relaxed; d=yahoo-inc.com; s=ginc1024; t=1439851746; bh=Zg0pSZvCcMHE9S9qpkoEKeacBIM4T3Xu4TUSMEL4rXw=; h=Date:From:Reply-To:To:Message-ID:Subject:MIME-Version:Content-Type; b=Ut+tXUluIOFrGnFm6m0fvXuIQDIEulXFkWmj9bQSO0JN3gPiWfuh1bFhZBdnu2C4SREtTfrxHI8q5DGPjD8yg4LnxFh3HOuaf4Ttm8w72QGO1HxJCdwkNvu5W4mnFTEB8hdl2u5naE4JqjJtM291ZYIJGvxFA2J3+Snj/N2aG40=
+X-YMail-OSG: G1B4VdwVM1lYA9kmxoxrGwEODiHeae6vbYVeBm754R2VWrC5KBM9pyd4ojSurOA
+ q0um_rXRvGr1aqpHntt5GL5mcITy4qZFZWIBKRlGdOvQKNsKMSzsglbrG0Io._.0dI8XBQ.DNWG3
+ Z5uVt9prZqJLlJG.FcGrNnYQTiX.Q0HDTID4rDKM.sA6Z_CUAPOto0IFqnA9buS5R8Rjy3xqs5qf
+ krxUdQCFbVG.ML8Kl0WJfy8ZKxjg1mT7Nma.ZOA--
+Received: by 98.138.105.251; Mon, 17 Aug 2015 22:49:05 +0000 
+Date: Mon, 17 Aug 2015 22:49:05 +0000 (UTC)
+From: Andres Erbsen <andreser@yahoo-inc.com>
+Reply-To: Andres Erbsen <andreser@yahoo-inc.com>
+To: Andres Erbsen Erbsen <andreser@yahoo-inc.com>
+Message-ID: <408588803.6263873.1439851745104.JavaMail.yahoo@mail.yahoo.com>
+Subject: end-to-end public key verification [test]
+MIME-Version: 1.0
+Content-Type: multipart/alternative; 
+	boundary="----=_Part_6263872_19047179.1439851745102"
+Content-Length: 622
+
+------=_Part_6263872_19047179.1439851745102
+Content-Type: text/plain; charset=UTF-8
+Content-Transfer-Encoding: 7bit
+
+fdsfasdfdasgawgasdgdsgadfgadsgdgadga
+------=_Part_6263872_19047179.1439851745102
+Content-Type: text/html; charset=UTF-8
+Content-Transfer-Encoding: 7bit
+
+<html><body><div style="color:#000; background-color:#fff; font-family:HelveticaNeue-Light, Helvetica Neue Light, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;font-size:16px"><div id="yui_3_16_0_1_1439835732243_26145" dir="ltr">fdsfasdfdasgawgasdgdsgadfgadsgdgadga</div></div></body></html>
+------=_Part_6263872_19047179.1439851745102--`, "\n", "\r\n", -1)
+
+func TestYahooIncDKIM(t *testing.T) {
+	_, err := ParseAndVerify(yahooIncDKIMtest, HeadersOnly, client)
+	if err != nil {
+		t.Fatal(err)
 	}
 }
